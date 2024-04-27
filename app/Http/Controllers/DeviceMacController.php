d89a59dfe27e184ec9732b81b58344e30b6fbd3b
Larson Carter <larsoncarter72404@gmail.com>
2024-04-27 16:37:08 -0500

    Comparison Module
    I made the API compare confirmed vs nonconfirmed MACs.

--- app/Http/Controllers/DeviceMacController.php
+++ app/Http/Controllers/DeviceMacController.php
@@ -7,23 +7,38 @@ use App\Models\NonConfirmedMac;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
+use App\Models\ConfirmedMac;
+
 class DeviceMacController extends Controller
 {
+
     public function store(Request $request)
     {
         try {
-            // Validate the request data if needed
-            // $request->validate([
-            //     'device_macs' => 'required|array',
-            //     'device_macs.*' => 'required|string|uuid', // Ensure each MAC address is a valid UUID
-            // ]);
-
-            // Your controller logic here
+            // Your existing controller logic here
             foreach ($request->all() as $device_mac) {
                 // Ensure each MAC address is a valid UUID
                 if (!Str::isUuid($device_mac)) {
                     throw new \InvalidArgumentException("Invalid UUID format: $device_mac");
                 }
+
+                // Check if the MAC address exists in the confirmed_mac table
+                $confirmed_mac = ConfirmedMac::where('device_mac', $device_mac)->first();
+
+//                if ($confirmed_mac) {
+//                    // If a match is found, return the matched MAC address with response code
+//                    return response()->json(['message' => 'Matching MAC address found in confirmed_mac table.', 'matched_mac' => $device_mac], 409);
+//                }
+
+                if ($confirmed_mac) {
+                    // If a match is found, return the matched MAC address and common_device_name with response code
+                    return response()->json([
+                        'message' => 'Matching MAC address found in confirmed_mac table.',
+                        'matched_mac' => $device_mac,
+                        'common_device_name' => $confirmed_mac->common_device_name,
+                    ], 409);
+                }
+
 
                 // Create NonConfirmedMac record with device MAC address
                 NonConfirmedMac::create([
@@ -40,6 +55,6 @@ class DeviceMacController extends Controller
             Log::error('Error occurred:', ['exception' => $e->getMessage()]);
             return response()->json(['error' => $e->getMessage()], 500);
         }
-
     }
+
 }
